65754ce9fb3f7acf3c43ab76ebc0387a67696100
Tomer Admon <tomer.a@qualisystems.com>
2022-10-20 10:06:19 +0300

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -29,10 +29,25 @@ resource "aws_s3_bucket_website_configuration" "bucket" {
   }
 }
 
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+
 resource "aws_s3_bucket_acl" "bucket" {
   bucket = aws_s3_bucket.bucket.id
 
   acl = "public-read"
+  
+    server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+      
 }
 
 resource "aws_s3_bucket_policy" "policy" {
